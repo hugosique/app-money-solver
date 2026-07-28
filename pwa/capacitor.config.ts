abacd77bb7dd0be6f74540ae6f0a9dc8e0a76e7b
hugosique <otahugo02@gmail.com>
2026-07-28 00:04:11 -0300

    setup: dependencies install

---
+++ pwa/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-money-solver',
+  webDir: 'www'
+};
+
+export default config;
